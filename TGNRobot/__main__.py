c6270433f13d7b3d6292e97726b42812870c4e68
Rishabh anand <90849358+rishabhanand2@users.noreply.github.com>
2021-11-17 20:34:44 +0530

    Update __main__.py

--- TGNRobot/__main__.py
+++ TGNRobot/__main__.py
@@ -694,7 +694,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "HELLO @admin KASE HO SAB")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "NOW LUCY IS WORKING")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
