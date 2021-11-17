c5d57b819e7879b52313517484b41d7ab4c91d9d
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-11-17 21:36:08 +0530

    Create Rishabh.py

---
+++ TGNRobot/modules/Rishabh.py
@@ -0,0 +1,8 @@
+from TGNRobot.events import register
+from TGNRobot import OWNER_ID
+from TGNRobot import telethn as tbot
+import os 
+
+@register(pattern="^/rishabh ?(.*)")
+async def rishabh(event):
+ message.reply_text(**Hey,I am the owner of this super Bot❤️🥳.**
